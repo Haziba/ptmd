37332ae5dcf7f382925475aa3bf4fb5e9d246ecb
Haziba <harry.boyes@gmail.com>
2019-08-11 21:50:51 +0100

    Wait until api is ready before starting e2e tests

--- e2e/PleaseTakeMyDogBackend.IntegrationTests/Fixture.cs
+++ e2e/PleaseTakeMyDogBackend.IntegrationTests/Fixture.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace PleaseTakeMyDogBackend.IntegrationTests
 {
-	public abstract class Fixture : IDisposable
-	{
-		private readonly IMessageSink messageSink;
-	    public Process Process { get; set; }
+    public abstract class Fixture : IDisposable
+    {
+        private readonly IMessageSink messageSink;
+        public Process Process { get; set; }
+        public bool ProcessStarting { get; set; }
 
-		protected Fixture(IMessageSink messageSink)
-		{
-			this.messageSink = messageSink;
+        protected Fixture(IMessageSink messageSink)
+        {
+            this.messageSink = messageSink;
 
-		    var dir = Directory.GetCurrentDirectory();
-		    var dirSplit = dir.Split('\\');
-		    var workingDir = string.Join('\\', dirSplit.Take(dirSplit.Length - 5));
-		    Console.WriteLine(workingDir);
+            var dir = Directory.GetCurrentDirectory();
+            var dirSplit = dir.Split('\\');
+            var workingDir = string.Join('\\', dirSplit.Take(dirSplit.Length - 5));
+            Console.WriteLine(workingDir);
 
             this.Process = new Process
             {
@@ -41,30 +40,37 @@ namespace PleaseTakeMyDogBackend.IntegrationTests
             this.Process.ErrorDataReceived += ProcessOnErrorDataReceived;
 
             this.Process.Start();
+            this.ProcessStarting = true;
 
             this.Process.BeginOutputReadLine();
             this.Process.BeginErrorReadLine();
 
-		    Thread.Sleep(3000);
-		}
+            while (this.ProcessStarting)
+                Thread.Sleep(100);
+        }
 
         private void ProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine(e.Data);
+            this.ProcessStarting = false;
         }
 
         private void ProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine(e.Data);
+
+            if (e.Data.Contains("Running on"))
+                this.ProcessStarting = false;
         }
 
-		public void Dispose()
-		{
-			this.messageSink.OnMessage(new DiagnosticMessage("Shutting down {0}", this.GetType().Name));
-		    this.Process.Dispose();
-			this.OnDisposing();
-		}
+        public void Dispose()
+        {
+            this.messageSink.OnMessage(new DiagnosticMessage("Shutting down {0}", this.GetType().Name));
+            this.Process.Kill();
+            this.Process.Dispose();
+            this.OnDisposing();
+        }
 
-		protected virtual void OnDisposing() => this.Process.Dispose();
-	}
+        protected virtual void OnDisposing() => this.Process.Dispose();
+    }
 }
